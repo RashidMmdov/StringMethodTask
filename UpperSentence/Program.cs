788b2916f33fb2df4b9ea294cdb5f7316878f233
RashidMmdov <tu7lw2f82@code.edu.az>
2022-03-15 01:43:54 +0400

    Add string method task

---
+++ UpperSentence/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UpperSentence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "bU tasKI metHodla yAzmAliSiz";
+            sentence= sentence.ToLower();
+            Console.WriteLine(CapitalLeterr(sentence));
+
+        }
+        public static string CapitalLeterr(string sentence)
+        {
+            sentence = sentence.Replace('b', 'B');
+            sentence = sentence.Replace("taski", "Taski");
+            sentence = sentence.Replace("methodla", "Methodla");
+            sentence = sentence.Replace('y', 'Y');
+
+            return sentence;
+        }
+    }
+    
+}
